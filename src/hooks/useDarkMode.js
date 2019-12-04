24c0760540c892c222767181c8ed0cb500fac853
Emily <bruner.emilym@gmail.com>
2019-12-04 15:14:40 -0500

    created darkmode hook

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
 
-const useDarkMode
+export const useDarkMode = (initialValue) => {
+    const [inputValue, setInputValue] = useLocalStorage(initialValue);
+    useEffect(() => {
+        const body = window.document.body;
+        return inputValue ? body.classList.add('dark-mode') : body.classList.remove('dark-mode')
+    }, [inputValue])
+    return [inputValue, setInputValue];
+}
